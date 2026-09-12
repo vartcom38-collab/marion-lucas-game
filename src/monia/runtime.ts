ec4f0bd9ab0fff5d57660cf005b5997d096e4845
vartcom38-collab <vartcom38@gmail.com>
2026-09-12 10:18:06 +0200

    feat: mount MonIA life media director

--- src/monia/runtime.ts
+++ src/monia/runtime.ts
@@ -5,6 +5,7 @@ import './early-game-phone-guard';
 import './iphone-shell';
 import './iphone-interactions';
 import './iphone-notifications';
+import './life-media-director';
 import { MARION_LUCAS_PROFILE, narrationPrompt, type MonIACompactContext } from './profile';
 import { moniaStorage } from './storage';
 import { askMonIAServerBrain } from './server-brain';
