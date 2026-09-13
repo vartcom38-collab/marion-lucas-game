86dd97557a99159ed60802ac5c8907e03e30c07c
vartcom38-collab <vartcom38@gmail.com>
2026-09-13 19:14:49 +0200

    Load long-term family memory echoes

--- src/monia/child-development-life.ts
+++ src/monia/child-development-life.ts
@@ -2,6 +2,7 @@ import './child-bond-life';
 import './trusted-nanny-life';
 import './family-visio-director';
 import './nanny-distance-update-director';
+import './family-memory-echo';
 import { getChildrenLife, type ChildLifeSnapshot } from './children-life';
 
 export type ChildDevelopmentPhase='early-care'|'preschool-rhythm'|'school-rhythm'|'preteen-autonomy'|'teen-autonomy'|'adult-independent';
