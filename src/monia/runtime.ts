15ed2070082195a2613b9a20d657dabf7be077ed
vartcom38-collab <vartcom38@gmail.com>
2026-09-13 11:51:39 +0200

    Load localized finca scene ambience

--- src/monia/runtime.ts
+++ src/monia/runtime.ts
@@ -12,6 +12,7 @@ import './media-life-layer';
 import './family-life-layer';
 import './property-agency-ui';
 import './finca-home-life';
+import './finca-scene-ambient';
 import './life-timeline-engine';
 import './rare-life-events';
 import { MARION_LUCAS_PROFILE, narrationPrompt, type MonIACompactContext } from './profile';
