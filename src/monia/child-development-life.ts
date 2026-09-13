d9a766558abee00560a415b8214dc2d7e455bd7c
vartcom38-collab <vartcom38@gmail.com>
2026-09-13 19:13:52 +0200

    Load nanny distance updates with family runtime

--- src/monia/child-development-life.ts
+++ src/monia/child-development-life.ts
@@ -1,6 +1,7 @@
 import './child-bond-life';
 import './trusted-nanny-life';
 import './family-visio-director';
+import './nanny-distance-update-director';
 import { getChildrenLife, type ChildLifeSnapshot } from './children-life';
 
 export type ChildDevelopmentPhase='early-care'|'preschool-rhythm'|'school-rhythm'|'preteen-autonomy'|'teen-autonomy'|'adult-independent';
