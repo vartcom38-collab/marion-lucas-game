16399958dcbb2736d16744f11112859c50874a23
vartcom38-collab <vartcom38@gmail.com>
2026-09-13 18:00:33 +0200

    Load guarded Lucas visio phone bridge

--- src/monia/living-social-runtime.ts
+++ src/monia/living-social-runtime.ts
@@ -1,5 +1,6 @@
 import './living-social-runtime.css';
 import './taurine-life-layer';
+import './visio-phone-bridge';
 import {getSocialPresence,setSocialPresence} from '../social-presence-state';
 import {getLucasPresence} from './lucas-presence-engine';
 
