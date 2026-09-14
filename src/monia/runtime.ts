00ab68e546d8f130f01d6e7ef27291b1d32f5b0a
vartcom38-collab <vartcom38@gmail.com>
2026-09-14 09:25:52 +0200

    Activate family residence continuity in runtime

--- src/monia/runtime.ts
+++ src/monia/runtime.ts
@@ -14,6 +14,7 @@ import './family-life-layer';
 import './france-spain-life-transition';
 import './spain-life-engine';
 import './madrid-home-life';
+import './residence-base-life';
 import './property-agency-ui';
 import './finca-home-life';
 import './finca-scene-ambient';
